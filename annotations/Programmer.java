95356374c1a0a42605328f34636432da79304f53
HAJAR <hajarharnane@gmail.com>
2022-03-13 14:49:22 +0100

    tp3

---
+++ annotations/Programmer.java
@@ -0,0 +1,11 @@
+package ma.education.tp.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Target(ElementType.TYPE)
+public @interface Programmer {
+	abstract int id();
+	String name();
+}
