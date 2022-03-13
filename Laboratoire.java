d0b59b3acf70bab05c432d522206698f78e7702f
Harnane <84943587+HajarHarnane@users.noreply.github.com>
2022-03-13 14:54:26 +0100

    Delete Laboratoire.java

--- Laboratoire.java
+++
@@ -1,9 +0,0 @@
-package ma.eduction.tp1.introduction;
-
-public class Laboratoire extends Salle{
-	String type;
-	public Laboratoire(long id, String nom, String type) {
-		super(id, nom);
-		this.type = type;
-		}
-}
